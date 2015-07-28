e120fee52892c4b43a805a9f197047956492661f
Ban <le.van.ban@framgia.com>
2015-07-28 20:44:30 +0700

    Finish report 07-2015 :D

--- app/src/main/java/com/example/rayleigh/monthlyreport_07_2015/MainActivity.java
+++ app/src/main/java/com/example/rayleigh/monthlyreport_07_2015/MainActivity.java
@@ -1,10 +1,11 @@
 package com.example.rayleigh.monthlyreport_07_2015;
 import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.hardware.Camera;
+import android.media.CamcorderProfile;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 
@@ -17,93 +18,84 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.Toast;
 import java.io.IOException;
 
+import static com.example.rayleigh.monthlyreport_07_2015.R.*;
 
+
+@SuppressWarnings("ALL")
 public class MainActivity extends Activity {
-    Button play, play_capture, stop, stop_capture, capture, record;
+    Button play, play_video, stop, stop_video, capture, record, record_video;
     private MediaRecorder myAudioRecorder;
-    private String outputFile = null;
+    private MediaRecorder mMediaRecorder;
+    private String outputFile3gp = null, outputFilemp4 = null;
     private Camera mCamera;
 
     private SurfaceView surfaceView;
     private SurfaceHolder surfaceHolder;
 
+    private Drawable logo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(layout.activity_main);
 
-        play=(Button)findViewById(R.id.button3);
-        stop=(Button)findViewById(R.id.button2);
-        record=(Button)findViewById(R.id.button);
-        play_capture = (Button)findViewById(R.id.play_capture);
-        stop_capture = (Button)findViewById(R.id.stop_capture);
-        capture = (Button)findViewById(R.id.capture);
+        play=(Button)findViewById(id.button3);
+        stop=(Button)findViewById(id.button2);
+        record=(Button)findViewById(id.button);
+        record_video=(Button)findViewById(id.record_video);
+        play_video = (Button)findViewById(id.play_video);
+        stop_video = (Button)findViewById(id.stop_video);
+        capture = (Button)findViewById(id.capture);
 
-        surfaceView = (SurfaceView)findViewById(R.id.surfaceView);
+        surfaceView = (SurfaceView)findViewById(id.surfaceView);
         surfaceHolder = surfaceView.getHolder();
-
+        logo = getResources().getDrawable(drawable.logo);
         stop.setEnabled(false);
         play.setEnabled(false);
-        stop_capture.setEnabled(false);
-        play_capture.setEnabled(false);
+        stop_video.setEnabled(false);
+        play_video.setEnabled(false);
 
-        myAudioRecorder=new MediaRecorder();
-
+        outputFile3gp = Environment.getExternalStorageDirectory().getAbsolutePath() + "/recording2.3gp";
 
         record.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                try {
-                    outputFile = Environment.getExternalStorageDirectory().getAbsolutePath() + "/recording.3gp";
-                    myAudioRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-                    myAudioRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-                    myAudioRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
-                    myAudioRecorder.setOutputFile(outputFile);
+            myAudioRecorder=new MediaRecorder();
+            myAudioRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            myAudioRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            myAudioRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+            myAudioRecorder.setOutputFile(outputFile3gp);
+            try {
+                myAudioRecorder.prepare();
+                myAudioRecorder.start();
+            }
+            catch (IllegalStateException | IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
 
-                    myAudioRecorder.prepare();
-                    myAudioRecorder.start();
-                }
+            record.setEnabled(false);
+            stop.setEnabled(true);
 
-                catch (IllegalStateException | IOException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-
-                record.setEnabled(false);
-                stop.setEnabled(true);
-
-                Toast.makeText(getApplicationContext(), "Recording started", Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "Recording started", Toast.LENGTH_LONG).show();
             }
         });
-
-        // Chụp màn hình
-        capture.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-                intent.putExtra(android.provider.MediaStore.EXTRA_SCREEN_ORIENTATION, ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-                startActivityForResult(intent, 0);
-            }
-        });
-
-
 
         stop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                myAudioRecorder.stop();
-                myAudioRecorder.release();
-                myAudioRecorder  = null;
+            myAudioRecorder.stop();
+            myAudioRecorder.release();
+            myAudioRecorder  = null;
 
-                stop.setEnabled(false);
-                play.setEnabled(true);
+            stop.setEnabled(false);
+            play.setEnabled(true);
 
-                Toast.makeText(getApplicationContext(), "Audio recorded successfully",Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "Audio recorded successfully",Toast.LENGTH_LONG).show();
             }
         });
 
@@ -113,23 +105,97 @@ public class MainActivity extends Activity {
                 MediaPlayer m = new MediaPlayer();
 
                 try {
-                    m.setDataSource(outputFile);
-                }
-
-                catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-                try {
+                    m.setDataSource(outputFile3gp);
                     m.prepare();
                 }
-
                 catch (IOException e) {
                     e.printStackTrace();
                 }
 
                 m.start();
                 Toast.makeText(getApplicationContext(), "Playing audio", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        record_video.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                record_video.setEnabled(false);
+                stop_video.setEnabled(true);
+                play_video.setEnabled(false);
+
+                outputFilemp4 = Environment.getExternalStorageDirectory().getAbsolutePath() + "/recording4.mp4";
+                surfaceView.setBackground(null);
+                mCamera = Camera.open();
+                mCamera.unlock();
+                myAudioRecorder = new MediaRecorder();
+                myAudioRecorder.setCamera(mCamera);
+                // Step 2: Set sources
+                myAudioRecorder.setAudioSource(MediaRecorder.AudioSource.CAMCORDER);
+                myAudioRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+                myAudioRecorder.setProfile(CamcorderProfile.get(CamcorderProfile.QUALITY_HIGH));
+                // Step 4: Set output file
+                myAudioRecorder.setOutputFile(outputFilemp4);
+                myAudioRecorder.setPreviewDisplay(surfaceHolder.getSurface());
+                try {
+                    myAudioRecorder.prepare();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                myAudioRecorder.start();
+            }
+        });
+
+        stop_video.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stop_video.setEnabled(false);
+                play_video.setEnabled(true);
+                myAudioRecorder.stop();
+                myAudioRecorder.reset();
+//                surfaceHolder.getSurface().
+                myAudioRecorder.release();
+                mCamera.release();
+                surfaceView.setBackground(logo);
+                stop.setEnabled(false);
+                play.setEnabled(true);
+            }
+        });
+
+        play_video.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MediaPlayer m = new MediaPlayer();
+                stop_video.setEnabled(false);
+                play_video.setEnabled(true);
+                surfaceView.setBackground(null);
+                surfaceHolder = surfaceView.getHolder();
+//                surfaceHolder = surfaceView.getHolder();
+                m.setDisplay(surfaceHolder);
+                try {
+                    m.setDataSource(outputFilemp4);
+                    m.setDisplay(surfaceHolder);
+                    m.prepare();
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                m.start();
+                Toast.makeText(getApplicationContext(), "Playing video", Toast.LENGTH_LONG).show();
+                stop.setEnabled(false);
+                play.setEnabled(false);
+            }
+        });
+
+
+        // Chụp màn hình
+        capture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+//            intent.putExtra(android.provider.MediaStore.EXTRA_SCREEN_ORIENTATION, ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                startActivityForResult(intent, 0);
             }
         });
     }
@@ -142,7 +208,6 @@ public class MainActivity extends Activity {
         Bitmap bp = (Bitmap) data.getExtras().get("data");
         BitmapDrawable ob = new BitmapDrawable(getResources(), bp);
         surfaceView.setBackground(ob);
-//        iv.setImageBitmap(bp);
     };
 
     @Override
